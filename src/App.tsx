1950f9590816ab68d6b470eafbef093229c357a1
min33sky <min33sky@naver.com>
2022-01-28 06:04:40 +0900

    :sparkles: Variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { GlobalStyle } from './styles/global';
-import { motion } from 'framer-motion';
+import { motion, Variants } from 'framer-motion';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -13,20 +13,67 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
+const Circle = styled(motion.div)`
+  place-self: center;
+
+  width: 70px;
+  height: 70px;
+  background-color: white;
+  border-radius: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.6);
+`;
+
+const boxVariants: Variants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: 'spring',
+      duration: 0.5,
+      bounce: 0.5,
+      //? 자식 애니메이션의 딜레이 설정
+      delayChildren: 0.5,
+      //? 자식간의 딜레이 설정
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariants: Variants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 function App() {
   return (
     <Wrapper>
       <GlobalStyle />
-      <Box
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateX: 360, rotateZ: 360 }}
-        transition={{ delay: 0.5, type: 'spring' }}
-      />
+
+      <Box variants={boxVariants} initial="start" animate="end">
+        {/* motion에서  제공되는 props들이 자식들에게도 모두 전달된다 */}
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
     </Wrapper>
   );
 }
