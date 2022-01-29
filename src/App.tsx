0aa05c692eef057f49d4ebfe3c397567ec8e87f8
min33sky <min33sky@naver.com>
2022-01-30 07:19:08 +0900

    :sparkles: Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { GlobalStyle } from './styles/global';
-import { AnimatePresence, motion, Variants } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { useState } from 'react';
 
 const Wrapper = styled(motion.div)`
@@ -14,80 +14,39 @@ const Wrapper = styled(motion.div)`
 
 const Box = styled(motion.div)`
   width: 400px;
-  height: 200px;
+  height: 400px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 100px;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 28px;
+  /* justify-content: center; */
+  /* align-items: center; */
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants: Variants = {
-  entry: (isBack: boolean) => ({
-    x: isBack ? -300 : 300,
-    opacity: 0,
-    scale: 0,
-  }),
-
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.3,
-    },
-  },
-
-  exit: (isBack: boolean) => ({
-    x: isBack ? 300 : -300,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 0.3,
-    },
-  }),
-};
+const Circle = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+  background-color: #00a5ff;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
 
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [isBack, setIsBack] = useState(false);
-
-  const nextItem = () =>
-    setVisible((prev) => {
-      setIsBack(false);
-      return prev === 10 ? 10 : prev + 1;
-    });
-
-  const prevItem = () =>
-    setVisible((prev) => {
-      setIsBack(true);
-      return prev === 1 ? 1 : prev - 1;
-    });
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => setClicked((prev) => !prev);
 
   return (
-    <Wrapper>
+    <Wrapper onClick={toggleClicked}>
       <GlobalStyle />
 
-      {/* 컴포넌트가 AnimatePresense 컴포넌트 내부에 있을 시 애니메이션 적용이 가능하다. */}
-      {/* exitBeforeEnter: 한 번에 하나의 컴포넌트만 랜더링 */}
-      <AnimatePresence custom={isBack}>
-        <Box
-          key={visible}
-          custom={isBack}
-          variants={boxVariants}
-          initial="entry"
-          animate="center"
-          exit="exit"
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={prevItem}>Prev</button>
-      <button onClick={nextItem}>Next</button>
+      <Box
+        style={{
+          justifyContent: clicked ? 'center' : 'flex-start',
+          alignItems: clicked ? 'center' : 'flex-start',
+        }}
+      >
+        <Circle layout />
+      </Box>
     </Wrapper>
   );
 }
