6ea5e1238c1979c0762d75089afe072f201c0cef
min33sky <min33sky@naver.com>
2022-02-09 09:00:44 +0900

    :sparkles: Drag Slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,56 @@
 import styled from 'styled-components';
 import { GlobalStyle } from './styles/global';
-import { AnimatePresence, motion } from 'framer-motion';
-import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { useEffect, useRef, useState } from 'react';
+import images from './libs/images';
 
-const Wrapper = styled(motion.div)`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
+const Carousel = styled(motion.div)`
+  margin: 10% 10%;
+  cursor: grab;
+  overflow: hidden;
 `;
 
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  width: 50vw;
+const InnerCarousel = styled(motion.div)`
+  display: flex;
+`;
 
-  div:first-child,
-  div:last-child {
-    grid-column: span 2;
+const Item = styled(motion.div)`
+  min-width: 25rem;
+  min-height: 40rem;
+  padding: 40px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 2rem;
+    pointer-events: none;
   }
-
-  gap: 10px;
-`;
-
-const Overlay = styled(motion.div)`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Box = styled(motion.div)`
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
-  const [boxId, setBoxId] = useState<string | null>(null);
+  const [width, setWidth] = useState(0);
+  const carouselRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (carouselRef.current) {
+      setWidth(carouselRef.current?.scrollWidth - carouselRef.current?.offsetWidth);
+    }
+  }, []);
 
   return (
-    <Wrapper>
+    <div>
       <GlobalStyle />
 
-      <Grid>
-        {[1, 2, 3, 4].map((item) => (
-          <Box key={item} onClick={() => setBoxId(String(item))} layoutId={String(item)} />
-        ))}
-      </Grid>
-
-      <AnimatePresence>
-        {boxId && (
-          <Overlay
-            onClick={() => setBoxId(null)}
-            initial={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
-            animate={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
-            exit={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
-          >
-            <Box layoutId={boxId} style={{ width: 200, height: 200 }} />
-          </Overlay>
-        )}
-      </AnimatePresence>
-    </Wrapper>
+      <Carousel ref={carouselRef} whileTap={{ cursor: 'grabbing' }}>
+        <InnerCarousel drag="x" dragConstraints={{ right: 0, left: -width }}>
+          {images.map((image) => (
+            <Item key={image} className="item">
+              <img src={image} alt="" />
+            </Item>
+          ))}
+        </InnerCarousel>
+      </Carousel>
+    </div>
   );
 }
 
---
+++ src/libs/images.ts
@@ -0,0 +1,10 @@
+import photo1 from '../assets/1.jpg';
+import photo2 from '../assets/2.jpg';
+import photo3 from '../assets/3.jpg';
+import photo4 from '../assets/4.jpg';
+import photo5 from '../assets/5.jpg';
+import photo6 from '../assets/6.jpg';
+
+const images = [photo1, photo2, photo3, photo4, photo5, photo6];
+
+export default images;
